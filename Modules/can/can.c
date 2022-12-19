6c9aeed1c1721623372c3b5cabccf3e0195f8368
Jacek <jaceman44@gmail.com>
2022-12-20 00:08:38 +0100

    Added dummy can; added clock config

---
+++ Modules/can/can.c
@@ -0,0 +1,128 @@
+/* Includes ------------------------------------------------------------------*/
+#include <stm32h7xx_hal_gpio.h>
+#include <stm32h7xx_hal_cortex.h>
+#include <stm32h7xx_hal_rcc.h>
+#include <stm32h7xx_hal_fdcan.h>
+#include "can.h"
+#include <leds/leds.h>
+
+FDCAN_FilterTypeDef sFilterConfig;
+FDCAN_TxHeaderTypeDef TxHeader;
+FDCAN_RxHeaderTypeDef RxHeader;
+uint8_t TxData_Node1_To_Node2[] = { 0x10, 0x32, 0x54, 0x76, 0x98, 0x00, 0x11,
+		0x22, 0x33, 0x44, 0x55, 0x66 };
+uint8_t RxData_From_Node2[12];
+
+FDCAN_HandleTypeDef hfdcan1;
+
+void Error_Handler2(void);
+
+void MX_FDCAN1_Init(void) {
+
+	/* USER CODE BEGIN FDCAN1_Init 0 */
+
+	/* USER CODE END FDCAN1_Init 0 */
+
+	/* USER CODE BEGIN FDCAN1_Init 1 */
+
+	/* USER CODE END FDCAN1_Init 1 */
+	hfdcan1.Instance = FDCAN1;
+	hfdcan1.Init.FrameFormat = FDCAN_FRAME_FD_BRS;
+	hfdcan1.Init.Mode = FDCAN_MODE_NORMAL;
+	hfdcan1.Init.AutoRetransmission = ENABLE;
+	hfdcan1.Init.TransmitPause = DISABLE;
+	hfdcan1.Init.ProtocolException = DISABLE;
+	hfdcan1.Init.NominalPrescaler = 1;
+	hfdcan1.Init.NominalSyncJumpWidth = 13;
+	hfdcan1.Init.NominalTimeSeg1 = 86;
+	hfdcan1.Init.NominalTimeSeg2 = 13;
+	hfdcan1.Init.DataPrescaler = 2;
+	hfdcan1.Init.DataSyncJumpWidth = 12;
+	hfdcan1.Init.DataTimeSeg1 = 12;
+	hfdcan1.Init.DataTimeSeg2 = 12;
+	hfdcan1.Init.MessageRAMOffset = 0;
+	hfdcan1.Init.StdFiltersNbr = 1;
+	hfdcan1.Init.ExtFiltersNbr = 0;
+	hfdcan1.Init.RxFifo0ElmtsNbr = 0;
+	hfdcan1.Init.RxFifo0ElmtSize = FDCAN_DATA_BYTES_8;
+	hfdcan1.Init.RxFifo1ElmtsNbr = 0;
+	hfdcan1.Init.RxFifo1ElmtSize = FDCAN_DATA_BYTES_8;
+	hfdcan1.Init.RxBuffersNbr = 1;
+	hfdcan1.Init.RxBufferSize = FDCAN_DATA_BYTES_12;
+	hfdcan1.Init.TxEventsNbr = 0;
+	hfdcan1.Init.TxBuffersNbr = 1;
+	hfdcan1.Init.TxFifoQueueElmtsNbr = 0;
+	hfdcan1.Init.TxFifoQueueMode = FDCAN_TX_FIFO_OPERATION;
+	hfdcan1.Init.TxElmtSize = FDCAN_DATA_BYTES_12;
+	if (HAL_FDCAN_Init(&hfdcan1) != HAL_OK) {
+		Error_Handler2();
+	}
+
+	/* Configure standard ID reception filter to Rx buffer 0 */
+	sFilterConfig.IdType = FDCAN_STANDARD_ID;
+	sFilterConfig.FilterIndex = 0;
+#if 0
+  sFilterConfig.FilterType = FDCAN_FILTER_DUAL; // Ignore because FDCAN_FILTER_TO_RXBUFFER
+#endif
+	sFilterConfig.FilterConfig = FDCAN_FILTER_TO_RXBUFFER;
+	sFilterConfig.FilterID1 = 0x2; // ID Node2
+#if 0
+  sFilterConfig.FilterID2 = 0x0; // Ignore because FDCAN_FILTER_TO_RXBUFFER
+#endif
+	sFilterConfig.RxBufferIndex = 0;
+	if (HAL_FDCAN_ConfigFilter(&hfdcan1, &sFilterConfig) != HAL_OK) {
+		Error_Handler2();
+	}
+
+	/* Start the FDCAN module */
+	if (HAL_FDCAN_Start(&hfdcan1) != HAL_OK) {
+		Error_Handler2();
+	}
+}
+
+void Can_testMessage(void) {
+	TxHeader.Identifier = 0x1;
+	TxHeader.IdType = FDCAN_STANDARD_ID;
+	TxHeader.TxFrameType = FDCAN_DATA_FRAME;
+	TxHeader.DataLength = FDCAN_DLC_BYTES_12;
+	TxHeader.ErrorStateIndicator = FDCAN_ESI_ACTIVE;
+	TxHeader.BitRateSwitch = FDCAN_BRS_ON;
+	TxHeader.FDFormat = FDCAN_FD_CAN;
+	TxHeader.TxEventFifoControl = FDCAN_NO_TX_EVENTS;
+	TxHeader.MessageMarker = 0x0; // Ignore because FDCAN_NO_TX_EVENTS
+	if (HAL_FDCAN_AddMessageToTxBuffer(&hfdcan1, &TxHeader,
+			TxData_Node1_To_Node2, FDCAN_TX_BUFFER0) != HAL_OK) {
+		Error_Handler2();
+	}
+
+	/* Send Tx buffer message */
+	if (HAL_FDCAN_EnableTxBufferRequest(&hfdcan1, FDCAN_TX_BUFFER0) != HAL_OK) {
+		Error_Handler2();
+	}
+
+	 /* Polling for transmission complete on buffer index 0 */
+	while(HAL_FDCAN_IsTxBufferMessagePending(&hfdcan1, FDCAN_TX_BUFFER0) == 1);
+//	CAN_TxHeader.StdId = 20;
+//	CAN_TxHeader.ExtId = 20;
+//	CAN_TxHeader.IDE = CAN_ID_STD;
+//	CAN_TxHeader.RTR = CAN_RTR_DATA;
+//	CAN_TxHeader.DLC = 8;
+//	uint8_t dane[8];
+//	dane[0] = 0xAA;
+//	dane[1] = 0xAA;
+//	for (int i = 2; i < 8; i++) {
+//		dane[i] = 0;
+//	}
+//	HAL_CAN_AddTxMessage(&hcan1, &CAN_TxHeader, dane, &CAN_TxMailbox);
+	Leds_toggleLed(LED4);
+}
+
+void Error_Handler2(void) {
+	/* USER CODE BEGIN Error_Handler_Debug */
+	/* User can add his own implementation to report the HAL error return state */
+	while (1) {
+		volatile uint8_t a = 0;
+		a = 1;
+	}
+	/* USER CODE END Error_Handler_Debug */
+}
--- Src/main.c
+++ Src/main.c
@@ -9,6 +9,10 @@
 #include <stm32h7xx_hal.h>
 #include <stm32h7xx_hal_conf.h>
 #include "leds/leds.h"
+#include "can/can.h"
+
+
+void Error_Handler3(void);
 
 // static UART_HandleTypeDef huart3;
 void SysTick_Handler(void)
@@ -19,16 +23,83 @@ void SysTick_Handler(void)
 int main(void)
 {
 	HAL_Init();
+	SystemClock_Config();
 
 	Leds_init();
+	MX_FDCAN1_Init();
 	Leds_welcomeFLash();
 
     /* Loop forever */
 	while(1)
 	{
-		Leds_toggleLed(LED5);
-		HAL_Delay(1000);
-		Leds_toggleLed(LED5);
-		HAL_Delay(1000);
+		Can_testMessage();
+		HAL_Delay(500);
+//		Leds_toggleLed(LED5);
+//		HAL_Delay(1000);
+//		Leds_toggleLed(LED5);
+//		HAL_Delay(1000);
 	}
+}
+
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Supply configuration update enable
+  */
+  HAL_PWREx_ConfigSupply(PWR_LDO_SUPPLY);
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE0);
+
+  while(!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {}
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_DIV1;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 4;
+  RCC_OscInitStruct.PLL.PLLN = 9;
+  RCC_OscInitStruct.PLL.PLLP = 2;
+  RCC_OscInitStruct.PLL.PLLQ = 3;
+  RCC_OscInitStruct.PLL.PLLR = 2;
+  RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_3;
+  RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOMEDIUM;
+  RCC_OscInitStruct.PLL.PLLFRACN = 3072;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+	  Error_Handler3();
+  }
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2
+                              |RCC_CLOCKTYPE_D3PCLK1|RCC_CLOCKTYPE_D1PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV1;
+  RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+	  Error_Handler3();
+  }
+}
+
+void Error_Handler3(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
 }
