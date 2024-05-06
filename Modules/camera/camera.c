4f6fd6856ded2f1c224302644e36e0e7fd421eae
KennyD5 <151067377+KennyD5@users.noreply.github.com>
2024-05-06 11:24:41 +0200

    Update camera.c

--- Modules/camera/camera.c
+++ Modules/camera/camera.c
@@ -1,72 +1,83 @@
 void handleCamera(uint8_t *data) {
-    // Sprawdzenie czy dane spełniają warunki i włączenie odpowiedniej diody
-    if (data[1] == 1) {
-        HAL_GPIO_WritePin(LED_PORT, LED1_PIN, GPIO_PIN_SET);
+    // Sprawdzenie czy dane spełniają warunki i włączenie odpowiedniej kamery
+    if (data[0] == 1) 
+    {
+        cam_bridge[0] = 1;
     }
     else
     {
-    	HAL_GPIO_WritePin(LED_PORT, LED1_PIN, GPIO_PIN_RESET);
+        cam_bridge[0] = 4;	
     }
-    if (data[1] == 2) {
-        HAL_GPIO_WritePin(LED_PORT, LED2_PIN, GPIO_PIN_SET);
-    }
-    else
-        {
-        	HAL_GPIO_WritePin(LED_PORT, LED2_PIN, GPIO_PIN_RESET);
-        }
-    if (data[1] == 3) {
-
-
-        HAL_GPIO_WritePin(LED_PORT, LED3_PIN, GPIO_PIN_SET);
-    }
-    else
-            {
-            	HAL_GPIO_WritePin(LED_PORT, LED3_PIN, GPIO_PIN_RESET);
-            }
-   if (data[2] == 1) {
-        HAL_GPIO_WritePin(LED_PORT, LED1_PIN, GPIO_PIN_SET);
+    
+    if (data[0] == 2) 
+    {
+        cam_bridge[0] = 2;
     }
     else
     {
-    	HAL_GPIO_WritePin(LED_PORT, LED1_PIN, GPIO_PIN_RESET);
+        cam_bridge[0] = 4;
     }
-    if (data[2] == 2) {
-        HAL_GPIO_WritePin(LED_PORT, LED2_PIN, GPIO_PIN_SET);
-    }
-    else
-        {
-        	HAL_GPIO_WritePin(LED_PORT, LED2_PIN, GPIO_PIN_RESET);
-        }
-    if (data[2] == 3) {
-
-
-        HAL_GPIO_WritePin(LED_PORT, LED3_PIN, GPIO_PIN_SET);
-    }
-    else
-            {
-            	HAL_GPIO_WritePin(LED_PORT, LED3_PIN, GPIO_PIN_RESET);
-            }
-   if (data[3] == 1) {
-        HAL_GPIO_WritePin(LED_PORT, LED1_PIN, GPIO_PIN_SET);
+    
+    if (data[0] == 3) 
+    {
+        cam_bridge[0] = 3;
     }
     else
     {
-    	HAL_GPIO_WritePin(LED_PORT, LED1_PIN, GPIO_PIN_RESET);
+        cam_bridge[0] = 4;
     }
-    if (data[3] == 2) {
-        HAL_GPIO_WritePin(LED_PORT, LED2_PIN, GPIO_PIN_SET);
+
+    if (data[1] == 1) 
+    {
+        cam_bridge[1] = 1;
     }
     else
-        {
-        	HAL_GPIO_WritePin(LED_PORT, LED2_PIN, GPIO_PIN_RESET);
-        }
-    if (data[3] == 3) {
-
-
-        HAL_GPIO_WritePin(LED_PORT, LED3_PIN, GPIO_PIN_SET);
+    {
+        cam_bridge[1] = 4;	
+    }
+    
+    if (data[1] == 2) 
+    {
+        cam_bridge[1] = 2;
     }
     else
-            {
-            	HAL_GPIO_WritePin(LED_PORT, LED3_PIN, GPIO_PIN_RESET);
-            }
+    {
+        cam_bridge[1] = 4;
+    }
+    
+    if (data[1] == 3) 
+    {
+        cam_bridge[1] = 3;
+    }
+    else
+    {
+        cam_bridge[1] = 4;
+    }
+
+    if (data[2] == 1) 
+    {
+        cam_bridge[2] = 1;
+    }
+    else
+    {
+        cam_bridge[2] = 4;	
+    }
+    
+    if (data[2] == 2) 
+    {
+        cam_bridge[2] = 2;
+    }
+    else
+    {
+        cam_bridge[2] = 4;
+    }
+    
+    if (data[2] == 3) 
+    {
+        cam_bridge[2] = 3;
+    }
+    else
+    {
+        cam_bridge[2] = 4;
+    }
 }
