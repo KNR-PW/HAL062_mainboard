b4768857eeba5c1499f159235cf5925e5448cf9b
Czechos02 <krystianczechowicz02@gmail.com>
2025-03-04 23:39:39 +0100

    Initial commit of new software branch

--- Modules/camera/camera.h
+++
@@ -1,9 +0,0 @@
-#ifndef MODULE_CAMERA_CAMERA_H
-#define MODULE_CAMERA_CAMERA_H
-
-#include <stm32h7xx_hal.h>
-
-void handleCamera(uint8_t *data);
-
-
-#endif // MODULE_CAMERA_CAMERA_H
--- README.md
+++
@@ -1,3 +0,0 @@
-# HAL062_mainboard
-
-`git clone --recurse-submodules -j8 https://github.com/KNR-PW/SUB-HAL-062-mainboard.git`
